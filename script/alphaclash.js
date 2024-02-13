3da23fab37a9455454b7a49514b251e28c278af6
Hasibul Islam <hasibulislam@Hasibs-Mac-Studio-M2-Max.local>
2024-02-14 00:02:20 +0100

    class remove and add in play button has been added in JS

--- script/alphaclash.js
+++ script/alphaclash.js
@@ -2,4 +2,17 @@
 
 function play() {
     console.log(`I am clicked`);
+
+    //step 1:  hide home screen
+    const homeSection = document.getElementById('home');
+    
+    homeSection.classList.add('hidden')
+
+
+    const playSection = document.getElementById('playground');
+    
+    playSection.classList.remove('hidden')
+
+
+
 }
